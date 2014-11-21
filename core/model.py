9cb0ab40f7f5b810fdbd23c194881eb9e6e37560
Juan Manuel Bermúdez Cabrera <juanbc@correo.das.vcl>
2014-11-21 10:17:01 -0500

    d

---
+++ core/model.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Juan Manuel Bermúdez Cabrera'
+
+
---
+++ createExe.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Juan Manuel Bermúdez Cabrera'
+
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+options = {
+    'build_exe': {
+        'includes': 'atexit'
+    }
+}
+
+executables = [
+    Executable('StartApp.py', base=base)
+]
+
+setup(name='Mario',
+      version='0.1',
+      description='Sample cx_Freeze PyQt4 script',
+      executables=executables
+      )
+
